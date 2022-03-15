0dcfd34a15268cda907e377704eac25546221822
Rafal Walkow <r.walkow@po.edu.pl>
2022-03-15 20:57:14 +0100

    Modify js code in script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
 const titleClickHandler = function(event){
-  console.log('Link was clicked!');
-  console.log(event);
+    console.log('Link was clicked!');
+    console.log(event);
+  
+    /* remove class 'active' from all article links  */
+    const activeArticles = document.querySelectorAll('article.active');
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
+    }
+  
+    /* add class 'active' to the clicked link */
+  
+    /* remove class 'active' from all articles */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* remove class 'active' from all article links  */
-
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
-
-  /* get 'href' attribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
-}
-
-const links = document.querySelectorAll('.titles a');
-
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-}
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
+  
+    /* get 'href' attribute from the clicked link */
+  
+    /* find the correct article using the selector (value of 'href' attribute) */
+  
+    /* add class 'active' to the correct article */
+  }
+  
+  const links = document.querySelectorAll('.titles a');
+  
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
+  
